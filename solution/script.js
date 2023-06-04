b6ff3060eac762f6dbf0b3b3a4e1d2360b566936
am-alibee <reachalibee@gmail.com>
2023-06-04 23:18:42 +0100

    new commit

--- solution/script.js
+++ solution/script.js
@@ -1,3 +1,4 @@
+//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
 
 // Your users should be able to: 
 
@@ -9,7 +10,7 @@
 //   - The year is in the future
 //   - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
 
-//dom selection
+//dom element selection
 
 const form = document.querySelector('.form');
 const day = document.querySelector('#day'); 
@@ -18,54 +19,54 @@ const year = document.querySelector('#year');
 
 
 
-const errorMessage = function(input, message){
-  input.previousElementSibling.style.color = 'var(--Light-red)';
-  input.style.borderColor = 'var(--Light-red)';
-  input.nextElementSibling.classList.remove('hidden');
-  input.nextElementSibling.textContent = message;
-}
-const validateInput = function(input, message){
-  const id = input.id;
-  const value = Number(input);
+// const errorMessage = function(input, message){
+//   input.previousElementSibling.style.color = 'var(--Light-red)';
+//   input.style.borderColor = 'var(--Light-red)';
+//   input.nextElementSibling.classList.remove('hidden');
+//   input.nextElementSibling.textContent = message;
+// }
+// const validateInput = function(input, message){
+//   const id = input.id;
+//   const value = Number(input);
 
-switch (input.id) {
-  case 'day':
-    if (!value > 0 && !value <= 31){
-      errorMessage(input, 'must be a valid day');
-    }
-    break;
-  case 'month':
-    if (!value > 0 && !value <= 12){
-      errorMessage(input, 'must be a valid month');
-    }
-    break;
-  case 'year':
-    if (!value > 0 && !value <= 31){
-      errorMessage(input, 'must be a valid day');
-    }
-    break;
-  default:
-    break;
-}
+// switch (input.id) {
+//   case 'day':
+//     if (!value > 0 && !value <= 31){
+//       errorMessage(input, 'must be a valid day');
+//     }
+//     break;
+//   case 'month':
+//     if (!value > 0 && !value <= 12){
+//       errorMessage(input, 'must be a valid month');
+//     }
+//     break;
+//   case 'year':
+//     if (!value > 0 && !value <= 31){
+//       errorMessage(input, 'must be a valid day');
+//     }
+//     break;
+//   default:
+//     break;
+// }
 
-}
+// }
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
 
-  document.querySelectorAll('input').forEach(input => {
-    if(!input.value){
-      errorMessage(input, 'This field is required');
-    }else{
-      console.log(input.value);
-      validateInput(input, 'message')
-    }
-  });
+//   document.querySelectorAll('input').forEach(input => {
+//     if(!input.value){
+//       errorMessage(input, 'This field is required');
+//     }else{
+//       console.log(input.value);
+//       validateInput(input, 'message')
+//     }
+//   });
 
   // form.reset();
-})
+// })
 
-const dateDiff = function(day, month, year){
+// const dateDiff = function(day, month, year){
   // const now = new Date();
   // const before = new Date(`${day} ${month} ${year}`);
   // const years = now.getFullYear() - before.getFullYear();
@@ -74,14 +75,21 @@ const dateDiff = function(day, month, year){
   
   // console.log(years, months, days);
 
-  const now = new Date().getTime();
-  console.log(now);
-  const before = new Date('9 5 2000');
-  l
+//   const now = new Date().getTime();
+//   console.log(now);
+//   const before = new Date('9 5 2000');
+//   l
 
-  
 
-}
+
+// }
 //get no of milisecs btw now and date in the past
 
-dateDiff(6, 8, 2002);
+// dateDiff(6, 8, 2002);
+
+const months = ['jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sept', 'oct', 'nov', 'dec']
+
+
+
+const date = new Date()
+console.log(months[date.getMonth()]);
---
+++ solution/solution.js
@@ -0,0 +1,63 @@
+// Users should be able to:
+
+// - View an age in years, months, and days after submitting a valid date through the form
+// - Receive validation errors if:
+//   - Any field is empty when the form is submitted
+//   - The day number is not between 1-31
+//   - The month number is not between 1-12
+//   - The year is in the future
+//   - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+// - View the optimal layout for the interface depending on their device's screen size
+// - See hover and focus states for all interactive elements on the page
+// - **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+const form = document.querySelector('form')
+const inputs = document.querySelectorAll('input')
+
+
+const displayMessage = function(input, message){
+  console.log(`${input.id} is required`);
+  input.previousElementSibling.style.color = 'var(--Light-red)';
+  input.style.borderColor = 'var(--Light-red)';
+  input.nextElementSibling.classList.remove('hidden');
+  input.nextElementSibling.textContent = message;
+}
+
+const validateInput = function(){
+  inputs.forEach(input => {
+    let value = Number(input.value)
+    if(!value){
+      
+      displayMessage(input, `${input.id} is required`)
+    }else{
+      if(input.id === 'day'){
+        if(value > 1 && value <= 31){
+          console.log('date is working');
+        }else{
+          console.log(`${input.id} no dey work 00`);
+        }
+      }else if(input.id === 'month'){
+        if(value > 1 && value <= 12){
+          console.log(`${input.id} working`);
+        }else{
+          console.log(`${input.id} is not working`);
+        }
+      }else if(input.id === 'year'){
+        if(value >= 1950 && value < new Date().getFullYear()){
+          console.log(`${input.id} is working`);
+        }else{
+          console.log(`${input.id} is not working`);
+        }
+        
+      }
+    }
+  })
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  if(validateInput()){
+    console.log('true');
+  }
+})
